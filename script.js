98479e4af37b532b4d9ab94b623622fda613f57c
Suraj Khanal <58101903+surjkhanal@users.noreply.github.com>
2021-10-14 14:07:06 +0530

    final

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let currencyForm = document.forms["currency-form"];
 let rates = {};
-let API = "http://api.exchangeratesapi.io/v1/latest?access_key=57ab796d4fc2a8bebf93fd9e3f525157&symbols=AED,BTC,JPY,INR,USD,QAR,EUR"
-fetch(API).then(result=>{
-    result.json().then(data=>{
-        rates = data.rates;        
-    })
-})
+let API = 'https://freecurrencyapi.net/api/v2/latest?apikey=f039b260-2bf0-11ec-b48b-818b8551d36b&base_currency=inr'
+
 
 let fromMenu = document.getElementById("c-from");
 let toMenu = document.getElementById("c-to");
@@ -16,46 +12,48 @@ currencyForm.addEventListener("submit",(event)=>{
     let currentC = event.target["c-from"].value;
     let targetC = event.target["c-to"].value;
 
-    let currentAmountInEuro =(1/rates[currentC])*currentValue;
-    result = currentAmountInEuro*rates[targetC]
-    if(result){
-        result = result.toPrecision(3)
-        resultBlock.innerText = currentValue+ " "+currentC +" : "+result+" "+targetC;
-        resultBlock.style.display = "block";
-    }
+    let API = 'https://freecurrencyapi.net/api/v2/latest?apikey=f039b260-2bf0-11ec-b48b-818b8551d36b&base_currency='+currentC
 
+    fetch(API).then(result=>{
+        result.json().then(data=>{
+            rates = data.data; 
+            console.log(rates);
+            result = rates[targetC]*currentValue;
+            if(result){
+                result = result.toPrecision(5)
+                console.log(result)
+                resultBlock.innerText = currentValue+ " "+currentC +" : "+result+" "+targetC;
+                resultBlock.style.display = "block";
+            }
+        })
+    })
 })
 
 
-let apiForList = "http://api.exchangeratesapi.io/v1/symbols?access_key=57ab796d4fc2a8bebf93fd9e3f525157";
+let showCaseSelect = document.querySelector("#compare-select-currency") ;
+let showCaseResult = document.querySelector("#showcase-result") ;
+showCaseSelect.addEventListener("change",(event) => {
 
-// fetch(apiForList).then((result)=>{
-//     if(result.status=="200"){
-//         result.json().then((data)=>{
-//             console.log(data);
-//             for (key in data.symbols){
-//                 let txt = key+" : " +data.symbols[key];
-//                 let newC = document.createElement("option");
-//                 newC.text= txt;
-//                 toMenu.appendChild(newC);
-//                 fromMenu.appendChild(newC.cloneNode(true));
-//             }
-//         })
-//     }
-// })
+    let API = 'https://freecurrencyapi.net/api/v2/latest?apikey=f039b260-2bf0-11ec-b48b-818b8551d36b&base_currency='+event.target.value;
+    showCaseResult.innerHTML = "";
+    fetch(API).then((response)=>{
 
-fetch(API).then((result)=>{
-    if(result.status=="200"){
-//         result.json().then((data)=>{
-//             console.log(data);
-//             for (key in data.symbols){
-//                 let txt = key+" : " +data.symbols[key];
-//                 let newC = document.createElement("option");
-//                 newC.text= txt;
-//                 toMenu.appendChild(newC);
-//                 fromMenu.appendChild(newC.cloneNode(true));
-//             }
-//         })
 
-    }
+        response.json().then((data)=>{
+            console.log(data)
+            let c=["EUR","INR","AED","JPY","USD","QAR","BTC"]
+            c.forEach(currency=>{
+                if(currency!==event.target.value){
+                    let child= document.createElement('span');
+                    child.className = "box";
+                    child.textContent =data.data[currency].toPrecision(5) +" "+currency;
+                    showCaseResult.appendChild(child);
+                }
+            })
+
+            
+        })
+    })
+
+
 })
